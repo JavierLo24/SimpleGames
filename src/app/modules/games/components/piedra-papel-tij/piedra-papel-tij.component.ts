ac6b409f6b9a07e155c1d355cb1eeac5bb8aed58
JavierLo24 <javierandres.123@hotmail.com>
2024-01-19 10:14:50 -0500

    Temporizador y puntajes añadidos

--- src/app/modules/games/components/piedra-papel-tij/piedra-papel-tij.component.ts
+++ src/app/modules/games/components/piedra-papel-tij/piedra-papel-tij.component.ts
@@ -8,9 +8,14 @@ import {OptionsGame} from '../../interfaces/options-game'
 })
 export class PiedraPapelTijComponent {
 
+  timeLeft: number = 5;
+  interval: any;
+  puntajeP1: number = 0;
+  puntajeP2: number = 0;
   public player1 : string = "";
   public player2 : string = "";
   public resultado: string = "A jugar";
+  public isDisabled: boolean = true;
   public showOption: OptionsGame[] = [
     {
     name: "papel",
@@ -26,31 +31,46 @@ export class PiedraPapelTijComponent {
     },
   ];
 
+  startTimer() {
+    this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.timeLeft = 5;
+      }
+    },1000)
+  }
+
   asignOption(option:string):void{
     this.player1 = option;
-    console.log(this.player1)
-
-  }
-pptEnAccion():void{
-  console.log(this.player2)
-  const randomIndex = Math.floor(Math.random() * this.showOption.length);
-  const opcionAleatoria = this.showOption[randomIndex];
-  this.player2 = opcionAleatoria.name
-  if(this.player1 == this.player2){
-    this.resultado = "Empate"
-  } else {
-    if (this.player1 == "papel" && this.player2 == "piedra" || this.player1 == "piedra" && this.player2 == "tijera" || this.player1 == "tijera" && this.player2 == "papel"){
-      this.resultado = "Gana el Jugador 1"
-    }else {
-      if (this.player1 == "tijera" && this.player2 == "piedra" || this.player1 == "piedra" && this.player2 == "papel" || this.player1 == "papel" && this.player2 == "tijera"){
-        this.resultado = "Gana el Jugador 2"
+    const randomIndex = Math.floor(Math.random() * this.showOption.length);
+    const opcionAleatoria = this.showOption[randomIndex];
+    this.player2 = opcionAleatoria.name
+    if(this.player1 == this.player2){
+      this.resultado = "Empate"
+    } else {
+      if (this.player1 == "papel" && this.player2 == "piedra" || this.player1 == "piedra" && this.player2 == "tijera" || this.player1 == "tijera" && this.player2 == "papel"){
+        this.resultado = "Gana el Jugador 1"
+        this.puntajeP1++;
+      }else {
+        if (this.player1 == "tijera" && this.player2 == "piedra" || this.player1 == "piedra" && this.player2 == "papel" || this.player1 == "papel" && this.player2 == "tijera"){
+          this.resultado = "Gana el Jugador 2"
+          this.puntajeP2++;
+        }
       }
     }
+    if (this.timeLeft == 0){
+      this.isDisabled = true;
+      clearInterval(this.interval);
+      this.timeLeft = 5;
+    }
   }
-}
 
-
-
-
+    pptEnAccion():void{
+      this.startTimer()
+      this.isDisabled = false;
+      this.puntajeP1 = 0;
+      this.puntajeP2 = 0;
+    }
 
 }
